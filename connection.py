c8135e5ecf5d1b520fe572b76b4dde55788091db
Bheema_Linga_Sai <kummarabheema@gmail.com>
2023-05-23 15:04:08 +0530

    connection.py

--- connection.py
+++ connection.py
@@ -1,3 +1,3 @@
 import mysql.connector
-mydb=mysql.connector.connect(host='localhost',user='root',password='Svbb@0808')
+mydb=mysql.connector.connect(host='localhost',user='root',password='Asdf@1234')
 print(mydb.connection_id)

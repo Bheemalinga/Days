d3c8066a81236180e534ba927fb82db89b421b24
Bheema_Linga_Sai <kummarabheema@gmail.com>
2023-05-23 15:04:59 +0530

    create_table.py

--- create_table.py
+++ create_table.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb=mysql.connector.connect(host='localhost',user='root',password='Samrin@1220',database='SAMRIN')
+mydb=mysql.connector.connect(host='localhost',user='root',password='Asdf@1234',database='SAM')
 cur=mydb.cursor()
 t= 'create table SWAP(emp_id integer(4),emp_name varchar(30),salary integer(10))'
 cur.execute(t)

6c8527faacc990b104c0aad5bce06866ce067b95
Bheema_Linga_Sai <kummarabheema@gmail.com>
2023-05-23 15:08:20 +0530

    update.py

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
 import mysql.connector
-mydb = mysql.connector.connect(host = "localhost",user="root",password="Svbb@0808",database="football")
+mydb = mysql.connector.connect(host = "localhost",user="root",password="Asdf@1234",database="football")
 cur=mydb.cursor()
 x='update football_venue'

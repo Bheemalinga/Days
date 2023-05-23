9443d54b68e39cfd0bc50416eecea8c6e7ccb072
Bheema_Linga_Sai <kummarabheema@gmail.com>
2023-05-23 15:08:49 +0530

    update_data.py

--- update_data.py
+++ update_data.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb=mysql.connector.connect(host='localhost',user='root',password='Samrin@1220',database='SAMRIN')
+mydb=mysql.connector.connect(host='localhost',user='root',password='Asdf@1234',database='SAM')
 cur=mydb.cursor()
 x='update SWAP set salary=salary+100 where emp_id=101'
 cur.execute(x)

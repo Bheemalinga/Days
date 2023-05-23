1d1395350e2e16cc89943914d3af60ca530e670a
Bheema_Linga_Sai <kummarabheema@gmail.com>
2023-05-23 15:06:23 +0530

    insert_data.py

--- insert_data.py
+++ insert_data.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb=mysql.connector.connect(host='localhost',user='root',password='Samrin@1220',database='SAMRIN')
+mydb=mysql.connector.connect(host='localhost',user='root',password='Asdf@1234',database='SAM')
 cur=mydb.cursor()
 z='insert into SWAP(emp_id,emp_name,salary) values(%s,%s,%s)'
 a=(100,'MUSSU',10000)
